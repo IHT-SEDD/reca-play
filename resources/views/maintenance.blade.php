811f02a3dfc923c35e87c24ce76bee4ce1e589c4
M Gibran Ramadhan <144303284+GibranRamadhannn@users.noreply.github.com>
2025-12-09 13:28:56 +0700

    Updates gibran

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -23,7 +23,7 @@
  <meta name="viewport" content="width=device-width, initial-scale=1">
  <meta name="csrf-token" content="{{ csrf_token() }}">
 
- <title>{{ config('app.name', 'RECA') }} - 504 Gateway Timeout</title>
+ <title>{{ config('app.name', 'RECA') }} - 503 Gateway Timeout</title>
  <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/logos/reca-black.png') }}">
 
  <!-- Meta SEO -->
